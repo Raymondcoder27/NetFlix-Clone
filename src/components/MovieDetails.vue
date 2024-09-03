61a274b2ce5d41c083df41d329d2639c1b9d76eb
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-03 14:32:10 +0300

    adds MovieDetails component

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -1,5 +1,20 @@
+<script setup>
+  const props = definProps({
+    movie
+  })
+</script>
+
 <template>
-  <div class="item">
-   
+  <div v-if="movie" class="z-20 text-white pt-20 w-[53%]">
+    <div class="font-semibold font-serif text-[55px]">
+      {{movie.name}}
+    </div>
+    <div class="flex items-center text-lg pt-4">
+      {{ movie.year }}
+    </div>
+    <div class="relative">
+      <div class="absolute text-[40px] left-[2px] -top-[12px]">.</div>
+      <div class="pl-4">{{movie.genre.one}}</div>
+    </div>
   </div>
 </template>
