4c5fa94bda6c5539c79159343b93563af731b59c
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-03 15:14:30 +0300

    adds VideoCarousel component

--- src/components/VideoCarousel.vue
+++ src/components/VideoCarousel.vue
@@ -1,6 +1,17 @@
-<script>
-
-</script>
+<script setup>
+    import {ref, toRefs} from 'vue'
+    import 'vue3-carousel/dist/carousel.css'
+    import { Carousel, Slide, Navigation } from 'vue3-carousel';
+</script>   
 <template>
     
 </template>
+
+<style>
+    .carousel_prev,
+    .carousel_next,
+    .carousel_prev:hover,
+    .carousel_next:hover{
+        color: white;
+    }
+</style>
