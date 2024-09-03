0039d2fbcae47f2e5f026d2586d27a008f39e509
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-03 15:09:35 +0300

    adds VideoCarousel component

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import Plus from 'vue-material-design-icons/Plus.vue'
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
 
 import MovieDetails from '@/components/MovieDetails.vue'
+import VideoCarousel from '@/components/VideoCarousel.vue'
 
 import {useMovieStore} from '@/stores/movie'
 import { storeToRefs } from 'pinia'
@@ -66,6 +67,8 @@ onMounted(()=>{
 
         <div class="fixed z-30 bottom-0 right-0 w-full overflow-y-auto h-[55%] pl-[120px]">
           <VideoCarousel class="pb-14 pt-14" category="Popular Movies" :movies="movies[0]" />
+          <VideoCarousel class="pb-14" category="Horror Movies" :movies="movies[1]" />
+          <VideoCarousel class="pb-32" category="Featured Movies" :movies="movies[2]" />
         </div>
       </div>
   </div>
