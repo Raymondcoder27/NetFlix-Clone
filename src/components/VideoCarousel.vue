7aac69cbbc3e1b33632e730874ad49ae971c2820
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-03 15:19:41 +0300

    adds VideoCarousel component

--- src/components/VideoCarousel.vue
+++ src/components/VideoCarousel.vue
@@ -2,6 +2,25 @@
     import {ref, toRefs} from 'vue'
     import 'vue3-carousel/dist/carousel.css'
     import { Carousel, Slide, Navigation } from 'vue3-carousel';
+
+    import {useMovieStore} from '@/stores/movie'
+    import { storeToRefs } from 'pinia';
+
+    const useMovie = useMovieStore()
+    const {movie, showFullMovie} = storeToRefs(useMovie)
+
+    let currentSlide = ref(0)
+    const props = defineProps({
+        category: String,
+        movies: Array
+    })
+    const {movies, category} = toRefs(props)
+
+    const currentSlideObject = (slide, index) => {
+        if (index === currentSlide.value){
+            movie.value = slide
+        }
+    }
 </script>   
 <template>
     
